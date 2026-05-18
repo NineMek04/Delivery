94160b73845b2bda14bcff8839e111cfea6dc1e2
Tanongsak.pu.intern <Tanongsak.pu.intern@BetimesSolutions.onmicrosoft.com>
2026-05-18 17:23:45 +0700

    feat: scaffold Customer and StorePartner components and implement main dashboard layout structure

---
+++ admin-dashboard/src/app/features/store-partner/store-partner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-partner',
+  imports: [],
+  templateUrl: './store-partner.component.html',
+  styleUrl: './store-partner.component.scss'
+})
+export class StorePartnerComponent {
+
+}
